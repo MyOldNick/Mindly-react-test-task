576580dbe3607a38676eb75011079f90782d8b40
MyOldNick <denis.zin1999@gmail.com>
2021-06-02 10:57:39 +0300

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+//react
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//redux
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchDisFavorite,
+  fetchFavorite,
+  fetchPsyhologists,
+} from "./store/asyncActions/psyhologists";
+import { fetchStatistics } from './store/asyncActions/statistics'
+//pages
+import MainPage from "./pages/MainPage";
+import AddPsyhologist from "./pages/AddPsyhologist";
+import Favorite from "./pages/Favorite";
+import DisFavorite from "./pages/DisFavorite";
+import Statistics from './pages/Statistics'
+//components
+import Navigation from "./components/NavBar";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const psyhologists = useSelector(
+    (state) => state.psyhologistsReducer.psyhologistsList
+  );
+
+  useEffect(() => {
+    if (psyhologists.length <= 0) {
+      dispatch(fetchPsyhologists());
+      dispatch(fetchFavorite());
+      dispatch(fetchDisFavorite());
+      dispatch(fetchStatistics())
+    }
+  }, [dispatch, fetchDisFavorite, fetchFavorite, fetchPsyhologists, fetchStatistics, psyhologists]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route path="/add" component={AddPsyhologist} />
+          <Route path="/favorite" component={Favorite} />
+          <Route path="/disfavorite" component={DisFavorite} />
+          <Route path="/statistics" component={Statistics} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
